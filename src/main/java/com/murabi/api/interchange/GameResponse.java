efd0cbc4b4b4dba4c534b653658c8740cd398517
IvanHristov98 <ivan.hristov.biz@gmail.com>
2021-01-16 17:36:25 +0200

    Handle docker localhost murker issue
    Co-authored-by: DimitarL <miteto66@gmail.com>

--- src/main/java/com/murabi/api/interchange/GameResponse.java
+++ src/main/java/com/murabi/api/interchange/GameResponse.java
@@ -1,6 +1,10 @@
 package com.murabi.api.interchange;
 
 public class GameResponse {
+    public static String DOCKER_LOCALHOST = "host.docker.internal";
+    public static String LOCALHOST = "localhost";
+    public static String IP_PORT_SPLITTER = ":";
+
     public String gameID;
     public String key;
     public String murkerAddr;
@@ -8,6 +12,16 @@ public class GameResponse {
     public GameResponse(String gameID, String key, String murkerAddr) {
         this.gameID = gameID;
         this.key = key;
-        this.murkerAddr = murkerAddr;
+        this.murkerAddr = prepareMurkerAddr(murkerAddr);
+    }
+
+    private String prepareMurkerAddr(String addr) {
+        String[] chunks = addr.split(IP_PORT_SPLITTER);
+
+        if (chunks[0].equals(DOCKER_LOCALHOST)) {
+            return LOCALHOST + IP_PORT_SPLITTER + chunks[1];
+        }
+
+        return addr;
     }
 }
